3ea23eaa81e0c5d816143149d837e64b3fbfc643
fora-heims <foraheims@gmail.com>
2021-12-15 16:33:50 -0800

    added React Router. Home page created, with Routes for / and /books working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+import Home from './views/Home/Home';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <h1>Library Catalog</h1>
+        <NavLink to="/"></NavLink>
+        <NavLink to="/books"></NavLink>
+        <Switch>
+          <Route path="/books">
+            <BookList />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
---
+++ src/views/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div>
+      <section>
+        <h2>Welcome to Dangalf&apos;s Library of Computer Science!</h2>
+        <p className="home">
+          Check out the amazing selection of books in our <a href="/books">catalog</a>.
+        </p>
+      </section>
+    </div>
+  );
+}
